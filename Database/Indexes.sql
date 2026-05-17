77939703df955a60357d2053191d729c440f34ab
auberamas <151127487+auberamas@users.noreply.github.com>
2026-05-17 18:57:42 +0100

    update indexes

--- Database/Indexes.sql
+++ Database/Indexes.sql
@@ -8,3 +8,10 @@ ON Artist(id_artist);
 
 CREATE INDEX idx_member
 ON Member_(id_member);
+
+CREATE INDEX idx_ar_member  
+ON Artwork_Reservation(id_member);
+
+CREATE INDEX idx_ar_artwork 
+ON Artwork_Reservation(id_artwork);
+ 
